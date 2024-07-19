a62283d8601967ac98e721f4cf0f09dfcfc79c86
Okayama Tomoki <tomoki.okayama.04@gmail.com>
2024-07-19 17:46:35 +0900

    3-1

--- src/初級JAVA/Start.java
+++ src/初級JAVA/Start.java
@@ -2,15 +2,12 @@ package 初級JAVA;
 
 public class Start {
 	public static void main(String[] args) {
-		MyFrame frame1=new MyFrame();
-		frame1.setLocation(200,100);
-		frame1.setSize(150,150);
-		MyFrame frame2=new MyFrame();
-		frame2.setLocation(350,200);
-		MyFrame frame3=new RectFrame();
-		frame3.setLocation(50,300);
-		MyFrame frame4=new RectFrame();
-		frame4.setLocation(50,300);
+		System.out.println("プログラムを実行します");
+		@SuppressWarnings("unused")
+		MyFrame frame1=new OvalTest();
+		@SuppressWarnings("unused")
+		MyFrame frame2=new RectTest();
+		
 	}
 
 }
